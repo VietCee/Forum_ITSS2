567473bd99bd6344a5bfc31329179cde8433d0e8
longhms <130201421+longhms@users.noreply.github.com>
2024-10-04 10:11:04 +0700

    userInfo

--- app/views/userInfo.php
+++ app/views/userInfo.php
@@ -18,7 +18,7 @@ if (isset($_SESSION['successMessage'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HomePage Form</title>
+    <title>UserInfo Form</title>
     <link rel="stylesheet" href="/public/css/homePage.css">
     <link rel="stylesheet" href="/public/css/post.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
